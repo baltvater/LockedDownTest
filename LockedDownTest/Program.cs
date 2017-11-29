6d78bd0a68677b3aaf969d885f4caa4a677f58cb
Bill Altvater <baltvater@comcast.net>
2017-11-28 19:50:44 -0600

    initial commit

---
+++ LockedDownTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LockedDownTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This mother is LOCKED DOWN !!!");
+            Console.Read();
+        }
+    }
+}
